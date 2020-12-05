417d7ba36fef9142f50c3f3b6865a4e3d9231c5c
wilus23 <w.lepkowski23@gmail.com>
2020-12-05 14:28:59 +0100

    Add button music

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const game = new Game(200);
 
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+function playPause(){
+    if(count == 0){
+        count = 1;
+        audio.play();
+        playPauseBTN.innerHTML = "Pause &#9208;"
+    }else{
+        count = 0;
+        audio.pause();
+        playPauseBTN.innerHTML = "Play &#9658"
+    }
+}
+
+function stop(){
+    playPause();
+    audio.pause();
+    audio.currentTime = 0;
+}
+
+
+
 //Music 
 
 // let audio = document.getElementById("audio");
