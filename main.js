bc65fb88d85bfb74c26d612610a367f7057e9f6f
wilus23 <w.lepkowski23@gmail.com>
2020-12-05 12:32:15 +0100

    Music Fix

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const game = new Game(200);
-let audio = document.getElementById("audio");
-audio.volume = .8;
+
+//Music 
+
+// let audio = document.getElementById("audio");
+// audio.volume = .8;
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+// if (!isChrome){
+//     $('#iframeAudio').remove()
+// }
+// else {
+//     $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+// }
